b0b3e63087759d2649302e7016c4225a1520fc24
212772527 <Satyendra.Kumar3@ge.com>
2021-07-10 13:39:38 +0530

    add SudokuGame

--- sudoku.py
+++ sudoku.py
@@ -57,3 +57,61 @@ class SudokuBoard(object):
         # return the constructed board
         return board
 
+class SudokuGame(object):
+    """
+    A Sudoku game, in charge of storing the state of the board and checking
+    whether the puzzle is completed
+    """
+
+    def __init__(self, board_file):
+        self.board_file = board_file
+        self.start_puzzle = SudokuBoard(board_file).board
+
+    # initialize the game_over flag and creates a copy of the board
+    def start(self):
+        self.game_over = False
+        self.puzzle = []
+        for i in range(9):
+            self.puzzle.append([])
+            for j in range(9):
+                self.puzzle[i].append(self.start_puzzle[i][j])
+
+    # checks board's rows, columns and 3X3 squares for win
+    def check_win(self):
+        for row in range(9):
+            if not self.__check_row(row):
+                return False
+        for column in range(9):
+            if not self.__check_column(column):
+                return False
+        for row in range(3):
+            for column in range(3):
+                if not self.__check_square(row, column):
+                    return False
+
+        self.game_over = True
+        return True
+
+    # checks if elements of block are in range 1-9
+    def __check_block(self, block):
+        return set(block) == set(range(1, 10));
+
+    # checks if a row is valid
+    def __check_row(self, row):
+        return self.__check_block(self.puzzle[row])
+
+    # checks if a column is valid
+    def __check_column(self, column):
+        return self.__check_block(
+            [self.puzzle[row][column] for row in range(9)]
+        )
+
+    # checks if a 3X3 block is valid
+    def __check_square(self, row, column):
+        return self.__check_block(
+            [
+                self.puzzle[r][c]
+                for r in range(row*3, (row+1)*3)
+                for c in range(column*3, (column+1)*3)
+            ]
+        )
